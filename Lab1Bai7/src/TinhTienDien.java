e9279606e3c13ac850cd152c12ea0d02120399c7
jtadd123 <dat.nv.65cntt@ntu.edu.vn>
2026-01-23 21:13:12 +0700

    Lab1-Thêm-Elearning

---
+++ Lab1Bai7/src/TinhTienDien.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TinhTienDien {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số điện sử dụng trong tháng: ");
+        int soDien = scanner.nextInt();
+
+        int tien;
+
+        if (soDien < 50) {
+            tien = soDien * 1000;
+        } else {
+            tien = 50 * 1000 + (soDien - 50) * 1200;
+        }
+
+        System.out.println("Tiền điện phải trả: " + tien + " đồng");
+
+        scanner.close();
+    }
+}
