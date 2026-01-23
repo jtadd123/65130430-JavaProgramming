74265a281328069526ea5bdd16112c1ff9e62776
jtadd123 <dat.nv.65cntt@ntu.edu.vn>
2026-01-23 21:03:50 +0700

    Lab1-Thêm-Elearning

---
+++ Lab1Bai6/src/PhuongTrinhBacHai.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class PhuongTrinhBacHai {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Nhập b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Nhập c: ");
+        double c = scanner.nextDouble();
+
+        if (a == 0) {
+            // Giải phương trình bậc nhất: bx + c = 0
+            if (b == 0) {
+                if (c == 0) {
+                    System.out.println("Phương trình có vô số nghiệm");
+                } else {
+                    System.out.println("Phương trình vô nghiệm");
+                }
+            } else {
+                double x = -c / b;
+                System.out.println("Phương trình bậc nhất có nghiệm x = " + x);
+            }
+        } else {
+            // Giải phương trình bậc hai
+            double delta = Math.pow(b, 2) - 4 * a * c;
+
+            if (delta < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            } else if (delta == 0) {
+                double x = -b / (2 * a);
+                System.out.println("Phương trình có nghiệm kép x = " + x);
+            } else {
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("Phương trình có 2 nghiệm phân biệt:");
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        }
+
+        scanner.close();
+    }
+}
